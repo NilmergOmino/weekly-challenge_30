011d63bd89223a55eac7fe6b5ea26274ae0481d5
NilmergOmino <nilmerg.omino@gmail.com>
2018-04-05 13:43:01 +0200

    nav, header and portfolio finished

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,27 @@
 import '../scss/style.scss';
 let $ = require('jquery');
+
+$(document).on('scroll', function(){
+    let scrollTop = $(document).scrollTop();
+    if (scrollTop > 5){
+        $('#nav').addClass('-sticky');
+    }
+    else{
+        $('#nav').removeClass('-sticky');
+    }
+})
+
+$('#trigger-menu').on('click', function(){
+    $('#menu').toggleClass('-toggle');
+})
+
+$(document).ready(function(){
+    $('._js-anchor').on('click', function(event){
+        $('#menu').removeClass('-toggle');
+        let target = this.hash.slice(1),
+            hash = this.hash,
+            targetTop = $('#'+target).offset().top;
+        $('html, body').animate({ scrollTop: targetTop }, 600, history.pushState(null,null,hash))
+        event.preventDefault();
+    })
+})
